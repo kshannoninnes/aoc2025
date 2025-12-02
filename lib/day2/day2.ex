7e1121c02170ac8745ce925be9840c1405e9026f
Kalon Shannon-Innes <k.shannoninnes@gmail.com>
2025-12-02 06:18:26 +0000

    Move range logic into get_ranges

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -4,7 +4,7 @@ defmodule Day2 do
   def part1(input) do
     input
     |> get_ranges()
-    |> Enum.flat_map(&get_ids_from_range/1)
+    |> Enum.flat_map(&Enum.to_list/1)
     |> Enum.filter(&invalid_part1?/1)
     |> Enum.sum()
   end
@@ -12,29 +12,29 @@ defmodule Day2 do
   def part2(input) do
     input
     |> get_ranges()
-    |> Enum.flat_map(&get_ids_from_range/1)
+    |> Enum.flat_map(&Enum.to_list/1)
     |> Enum.filter(&invalid_part2?/1)
     |> Enum.sum()
   end
 
   defp invalid_part1?(id), do: Integer.to_string(id) |> repeat_twice?()
-  defp invalid_part2?(id), do: Integer.to_string(id)|> repeat_many?()
+  defp invalid_part2?(id), do: Integer.to_string(id) |> repeat_many?()
 
-  defp get_ranges(input), do: String.split(input, ",")
-
-  defp get_ids_from_range(range_string) do
-    [a, b] = String.split(range_string, "-")
-    a = String.to_integer(a)
-    b = String.to_integer(b)
-    Enum.to_list(a..b)
+  defp get_ranges(input) do
+    input
+    |> String.split(",")
+    |> Enum.map(fn str ->
+      [a, b] = String.split(str, "-")
+      String.to_integer(a)..String.to_integer(b)
+    end)
   end
 
-  def repeat_twice?(s) do
-    len = String.length(s)
+  defp repeat_twice?(str) do
+    len = String.length(str)
     half = div(len, 2)
 
     rem(len, 2) == 0 and
-    String.slice(s, 0, half) == String.slice(s, half, half)
+      String.slice(str, 0, half) == String.slice(str, half, half)
   end
 
   defp repeat_many?(str) do
