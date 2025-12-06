9c024282a81fec17d85f7c75fa4bcdd9bf3d8e6c
Kalon Shannon-Innes <k.shannoninnes@gmail.com>
2025-12-06 07:56:04 +0000

    Simplify to_int_list

--- lib/day6/day6.ex
+++ lib/day6/day6.ex
@@ -53,9 +53,7 @@ defmodule Day6 do
   defp eval({"+", operands}), do: Enum.sum(operands)
 
   defp to_int_list(string_list) do
-    string_list
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&String.to_integer/1)
+    Enum.map(string_list, &String.to_integer/1)
   end
 
   # Transpose digits from row major to column major
